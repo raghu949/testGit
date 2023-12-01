c974f3df634a199cffe8b824c7cd9d545f183ab9
raghu949 <raghurama2@gmail.com>
2023-12-01 11:22:56 +0530

    Addition

---
+++ calculatorBasic.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+######################################
+#Author:Raghu Ram N
+#Date:01/12/2023
+#Purpose: practice Git commands by using this file 
+#I will add different lines for practice
+################################################
+#Addition feature
+a=10
+b=20
+c=$(($a + $b)) 
+echo "Sum of two no is:" $c
